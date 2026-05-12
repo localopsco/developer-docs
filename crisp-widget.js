7050cea82981a79742a722a7faf498310f1eb52d
localops-root <anand@localops.co>
2026-05-12 20:34:46 +0530

    update last release notes

---
+++ crisp-widget.js
@@ -0,0 +1,1 @@
+window.$crisp = []; window.CRISP_WEBSITE_ID = "bed117a1-a943-40b8-bdc6-87a054466820"; (function () { d = document; s = d.createElement("script"); s.src = "https://client.crisp.chat/l.js"; s.async = 1; d.getElementsByTagName("head")[0].appendChild(s); })();
